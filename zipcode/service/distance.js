1f38663a506e137d695a0b33994c3ac49d223d82
SYED ALI <syed.ali@trivago.com>
2018-12-18 15:20:08 +0100

    nodejs based zipcode service

---
+++ zipcode/service/distance.js
@@ -0,0 +1,21 @@
+var request = require('request');
+
+const apiKey = process.env.ZIPCODE_API_KEY || "XlHaDNiGAZpY8LYdLC6kreMIIN09EsMPeNqPOq7sJpD5xykFEHSYMPWaeRBLjCHh";
+const zipCodeURL = 'https://www.zipcodeapi.com/rest/';
+
+var distance = {
+    find: function(req,res,next) {
+        request(zipCodeURL + apiKey + '/distance.json/' + req.params.zipcode1 + '/' + req.params.zipcode2 + '/mile', function(error,response,body){
+            if(!error && response.statusCode == 200) {
+                response  = JSON.parse(body);
+                res.send(response);
+            }
+            else {
+                console.log(response.statusCode + response.body);
+                res.send({distance:-1});
+            }
+        });
+    }
+};
+
+module.exports = distance;
